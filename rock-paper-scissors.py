001b342ca8567e50ae3f9d6337fe8c4d0077f411
Michelle <Michelli@users.noreply.github.com>
2020-06-30 16:09:40 +0200

    🎲 Rock, Paper, Scissors

---
+++ rock-paper-scissors.py
@@ -0,0 +1,23 @@
+import random
+import re
+
+player_one = str(input("Rock, paper, or scissors? ")).lower()
+
+def game(one):
+    hands = ["rock", "paper", "scissors"]
+    two = hands[random.randint(0,2)]
+
+    if re.match(r"^(?:rock|paper|scissors)$", one):
+        print(f"You chose {one}.\nI picked {two}.\n")
+
+        if one == two:
+            print("It's a tie!\n")
+        elif (one == "rock" and two == "scissors") or (one == "paper" and two == "rock") or (one == "scissors" and two == "paper"):
+            print("You win!\n")
+        else:
+            print("You lose!\n")
+        
+    else:
+        print("That's not a valid option.\n")
+
+game(player_one)
